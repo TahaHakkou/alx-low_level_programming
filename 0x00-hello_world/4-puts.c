3b330028b2b32660d23ca56a3dc594f94c65001e
TahaHakkou <hakkoutaha16@gmail.com>
2023-02-16 12:36:43 -0800

    4-puts.c Created

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main(void){
+/**
+ * main - prints "Programming is like building a multilingual puzzle
+ * Return : value 0
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
