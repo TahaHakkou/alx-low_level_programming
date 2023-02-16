7bf4896192ba212b3d8af261f524990bba1c3c1f
TahaHakkou <hakkoutaha16@gmail.com>
2023-02-16 13:52:35 -0800

    101-quote.c Created

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
- * main - prints "and that piece of art is useful\" - Dora Korpar, 2015-10-19" to stderr
+ * main - prints "and that piece of art is useful\" \
+ * 		  - Dora Korpar, 2015-10-19" to stderr
  * Return: value 1
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",1,59,stderr);
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", \
+		1, 59, stderr);
 	return (1);
 }
