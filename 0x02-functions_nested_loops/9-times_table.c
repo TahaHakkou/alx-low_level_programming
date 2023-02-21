48fb27ac7cea9948465d990f4f83402c86a9e37a
TahaHakkou <hakkoutaha16@gmail.com>
2023-02-21 15:50:18 -0800

    9-times_table Created

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,7 +15,7 @@ void times_table(void)
 			{
 				_putchar('0');
 			}
-			if (p / 10 == 0)
+			else if (p / 10 == 0)
 			{
 				_putchar(' ');
 				_putchar(' ');
@@ -27,7 +27,10 @@ void times_table(void)
 				_putchar(p / 10 + '0');
 				_putchar(p % 10 + '0');
 			}
-			_putchar(',');
+			if (j != 9)
+			{
+				_putchar(',');
+			}
 		}
 		_putchar('\n');
 	}
