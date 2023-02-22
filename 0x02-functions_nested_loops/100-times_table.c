9cbf0c6566354d2531a1078bf85ca334f3c2bec8
TahaHakkou <hakkoutaha16@gmail.com>
2023-02-22 02:37:23 -0800

    100-times_table.c Created

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * times_table - prints times table of n
+ * print_times_table - prints times table of n
  * @n: number
  */
 void print_times_table(int n)
@@ -22,26 +22,24 @@ void print_times_table(int n)
 				{
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(' ');
 					_putchar(p + '0');
 				}
 				else if (p / 100 == 0)
 				{
-					_putchar(' ');
 					_putchar(' ');
 					_putchar(p / 10 + '0');
 					_putchar(p % 10 + '0');
 				}
 				else
 				{
-					_putchar(' ');
 					_putchar(p / 100 + '0');
-					_putchar(p / 10 + '0');
+					_putchar((p / 10) % 10 + '0');
 					_putchar(p % 10 + '0');
 				}
-				if (j != 9)
+				if (j != n)
 				{
 					_putchar(',');
+					_putchar(' ');
 				}
 			}
 			_putchar('\n');
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,6 +7,8 @@
  */
 int print_last_digit(int n)
 {
-	_putchar((n * (-1)) % 10 + '0');
-	return ((n * (-1)) % 10);
+	if (n < 0)
+		n = n * (-1);
+	_putchar(n % 10 + '0');
+	return (n % 10);
 }
