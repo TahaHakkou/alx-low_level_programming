fad98724326049ab4a52cf604158b7d49f96ec2f
TahaHakkou <hakkoutaha16@gmail.com>
2023-02-22 03:14:00 -0800

    101-natural.c Created

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - sum of multiples of 3 & 5 below 1024
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i = 3;
+	int sum = 0;
+	
+	while (i < 1024)
+	{
+		sum += i;
+		i += 3;
+	}
+	i = 5;
+	while (i < 1024)
+	{
+		if (i % 3 != 0)
+			sum += i;
+		i += 5;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,7 +8,14 @@
 int print_last_digit(int n)
 {
 	if (n < 0)
-		n = n * (-1);
-	_putchar(n % 10 + '0');
-	return (n % 10);
+	{
+		n = (n + 1) * (-1);
+		_putchar(n % 10 + '1');
+		return (n % 10 + 1);
+	}
+	else
+	{
+		_putchar(n % 10 + '0');
+		return (n % 10);
+	}
 }
