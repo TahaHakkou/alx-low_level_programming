5b7843532fd611b923c0da462a34f502b1dab5b2
TahaHakkou <hakkoutaha16@gmail.com>
2023-02-21 16:20:58 -0800

    11-print_to_98.c Created

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,8 +13,11 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			if (i == 98)
+			{
 				printf("98");
-			printf("%d, ",i);
+				break;
+			}
+			printf("%d, ", i);
 		}
 	}
 	else
@@ -22,8 +25,11 @@ void print_to_98(int n)
 		for (i = n; i >= 98; i--)
 		{
 			if (i == 98)
+			{
 				printf("98");
-			printf("%d, ",i);
+				break;
+			}
+			printf("%d, ", i);
 		}
 	}
 	_putchar('\n');
