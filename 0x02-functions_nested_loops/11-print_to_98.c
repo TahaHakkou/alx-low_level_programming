feb71c819f9ebe570ff5f383e4d7d3664fdd56df
TahaHakkou <hakkoutaha16@gmail.com>
2023-03-05 02:41:52 -0800

    c 0x02 correct_2

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,31 +6,25 @@
  */
 void print_to_98(int n)
 {
-	int i;
+	int i, j;
 
 	if (n <= 98)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			if (i == 98)
-			{
-				printf("98");
-				break;
-			}
-			printf("%d, ", i);
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
 		}
-	}
-	else
+	} else if (n >= 98)
 	{
-		for (i = n; i >= 98; i--)
+		for (j = n; j >= 98; j--)
 		{
-			if (i == 98)
-			{
-				printf("98");
-				break;
-			}
-			printf("%d, ", i);
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
 		}
 	}
-	_putchar('\n');
 }
