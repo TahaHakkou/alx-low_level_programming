685b252d0480d3011c3753b6fd884b97db358bab
TahaHakkou <hakkoutaha16@gmail.com>
2023-03-06 04:45:11 -0800

    c 0x07 commit

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: string destination
+ * @to: string source
+ */
+void set_string(char **s, char *to)
+{
+	s = to;
+}
---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string
+ * @accept: acceptable bytes
+ *
+ * Return: the number of bytes in the initial segment of s which consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i;
+	int j, found;
+
+	found = 0;
+	i = 0;
+	while (found && s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != s[i] && accept[j] != '\0')
+			j++;
+		if (s[i] == accept[j - 1])
+			found = 1;
+		else
+			found = 0;
+		i++;
+	}
+	return (i);
+}
