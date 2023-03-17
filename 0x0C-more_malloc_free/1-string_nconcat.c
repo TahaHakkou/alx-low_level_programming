2f303869106ea20b6b9465667c647063daeb1dda
TahaHakkou <hakkoutaha16@gmail.com>
2023-03-16 17:11:50 -0700

    c 0x0c correct_1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,7 +26,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		memcpy(s, s1, strlen(s1));
 		memcpy(s + strlen(s1), s2, n);
-		memcpy(s + strlen(s1) + n, '\0', 1);
+		memcpy(s + strlen(s1) + n, "", 1);
 	}
 	return (s);
 }
