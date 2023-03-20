f3f01f6c8c96e64195032b7a92699591aa43a8c4
TahaHakkou <hakkoutaha16@gmail.com>
2023-03-20 16:48:53 -0700

    c 0x0d correct_1.3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
 #ifndef FL_MACRO_H
 #define FL_MACRO_H
-#define ABS(x) ((x) >= (0) ? (x) : (-x))
+#define ABS(x) ((x) == (INT_MIN) ? -(long int)(x) : ((x) >= (0) ? (x) : (-x)))
 #endif /* FL_MACRO_H */
