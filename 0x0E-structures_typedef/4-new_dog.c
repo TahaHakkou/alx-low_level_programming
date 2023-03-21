df031c7d6ca8b0c8b3fd2d354d4dd40fcc9cf08f
TahaHakkou <hakkoutaha16@gmail.com>
2023-03-20 17:03:59 -0700

    c 0x0e correct_1.4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,8 +17,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d = malloc(sizeof(*d));
 	if (d == NULL)
 		return (NULL);
-	d->name = name;
+	strcpy(d->name, name);
 	d->age = age;
-	d->owner = owner;
+	strcpy(d->owner, owner);
 	return (d);
 }
