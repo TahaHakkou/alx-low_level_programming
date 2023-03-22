95989f097ee6a7e3d7d37482db4b99603d04979b
TahaHakkou <hakkoutaha16@gmail.com>
2023-03-22 04:56:54 -0700

    c 0x0f  precommit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,6 @@
 #include "dog.h"
-#include <stdio.h>
 #include <stddef.h>
+#include <stdlib.h>
 /**
  * init_dog - initialize a variable of type struct dog
  * @d: a pointer to the dog instance
@@ -10,15 +10,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
-	else
-	{
-		printf("Ok\n");
-		exit(0);
-	}
+	if (d == NULL)
+		d = malloc(sizeof(*d));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
